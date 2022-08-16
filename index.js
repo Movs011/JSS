85576efd6495a51de22ff3c792e10966508479fd
Movsar011 <apple_id999_movs@mail.ru>
2022-08-16 19:01:43 +0300

    JS

---
+++ index.js
@@ -0,0 +1,74 @@
+// console.log ("Hello,world")
+// console.log(2+3)
+// console.log(10-2)
+// console.log(2*3);
+// console.log(10/5);
+// console.log(2**3);
+// console.log((2+3)+(10-2)+(2*3)+(10/5)+(2**3));
+
+// let, const
+// let n1 = 1+2
+// let n2 = 2-1
+// let n3 = 4*2
+// let n4 = 6/3
+// let n5 = 2**3
+// console.log(n1+n2+n3+n4+n5);
+// n1 = 10-2
+// console.log(n1+n2+n3+n4+n5);
+
+// Типы данных JS
+// let num = 1 //number
+
+// let str1 = "Me name Movsar" // string
+// let str2 = 'Me name Movsar'
+// let str3 = `Me name Movsar`
+// console.log(str3);
+
+// let bool1 = true // boolean
+// let bool2 = false
+
+// str3 = `Me name Mansur`
+// console.log(str3);
+
+//создайте 5 переменных number
+//создайте 5 переменных string
+//Перезапищите 2-3 переменных number и 2-3 переменных string
+
+let n1 = 32+2
+let n2 = 55-43
+let n3 = 5*3
+let n4 = 10/2
+let n5 = 2**5
+
+let str1 = 'Said'
+let str2 = 'Movsar'
+let str3 = 'Adam'
+let str4 = 'Movlid'
+let str5 = 'Shamil'
+console.log(n1, n2, n3, n4, n5);
+console.log(str1, str2, str3, str4, str5);
+
+n2 = 4-1
+n5 = 5*5
+n3= 3**3
+
+str1 = 'Brat'
+str2 = 'King'
+str3 = 'Eva'
+console.log(n1, n2, n3, n4, n5);
+console.log(str1, str2, str3, str4, str5);
+
+//          0 1 2 3   4 5
+let nums = [1,2,5,10,-3,2]
+
+//              0       1        2        3
+let names = ['Movsar','Said', 'Ibra', 'Movlid']
+
+//создайте массив имен
+//создайте массив типа ["Имя" возраст]["Movsar", 25, Adam, 27...]
+let nameAge = ['Movsar', 32, 'Said', 11, 'Ibra', 15, 'Muhammad', 44, 'Adlan', 27]
+console.log("Имя:",nameAge[0], ",","Возраст:", nameAge[1]);
+console.log("Имя:",nameAge[2], ",","Возраст:", nameAge[3]);
+console.log("Имя:",nameAge[4], ",","Возраст:", nameAge[5]);
+console.log("Имя:",nameAge[6], ",","Возраст:", nameAge[7]);
+console.log("Имя:",nameAge[8], ",","Возраст:", nameAge[9]);
