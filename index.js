6096f203c18c133fe67ed1c10f53460d0e88322e
Movsar011 <apple_id999_movs@mail.ru>
2022-08-20 12:05:24 +0300

    JS2

--- index.js
+++ index.js
@@ -34,41 +34,106 @@
 //создайте 5 переменных string
 //Перезапищите 2-3 переменных number и 2-3 переменных string
 
-let n1 = 32+2
-let n2 = 55-43
-let n3 = 5*3
-let n4 = 10/2
-let n5 = 2**5
+// let n1 = 32+2
+// let n2 = 55-43
+// let n3 = 5*3
+// let n4 = 10/2
+// let n5 = 2**5
 
-let str1 = 'Said'
-let str2 = 'Movsar'
-let str3 = 'Adam'
-let str4 = 'Movlid'
-let str5 = 'Shamil'
-console.log(n1, n2, n3, n4, n5);
-console.log(str1, str2, str3, str4, str5);
+// let str1 = 'Said'
+// let str2 = 'Movsar'
+// let str3 = 'Adam'
+// let str4 = 'Movlid'
+// let str5 = 'Shamil'
+// console.log(n1, n2, n3, n4, n5);
+// console.log(str1, str2, str3, str4, str5);
 
-n2 = 4-1
-n5 = 5*5
-n3= 3**3
+// n2 = 4-1
+// n5 = 5*5
+// n3= 3**3
 
-str1 = 'Brat'
-str2 = 'King'
-str3 = 'Eva'
-console.log(n1, n2, n3, n4, n5);
-console.log(str1, str2, str3, str4, str5);
+// str1 = 'Brat'
+// str2 = 'King'
+// str3 = 'Eva'
+// console.log(n1, n2, n3, n4, n5);
+// console.log(str1, str2, str3, str4, str5);
 
 //          0 1 2 3   4 5
-let nums = [1,2,5,10,-3,2]
+// let nums = [1,2,5,10,-3,2]
 
-//              0       1        2        3
-let names = ['Movsar','Said', 'Ibra', 'Movlid']
+// //              0       1        2        3
+// let names = ['Movsar','Said', 'Ibra', 'Movlid']
 
-//создайте массив имен
-//создайте массив типа ["Имя" возраст]["Movsar", 25, Adam, 27...]
-let nameAge = ['Movsar', 32, 'Said', 11, 'Ibra', 15, 'Muhammad', 44, 'Adlan', 27]
-console.log("Имя:",nameAge[0], ",","Возраст:", nameAge[1]);
-console.log("Имя:",nameAge[2], ",","Возраст:", nameAge[3]);
-console.log("Имя:",nameAge[4], ",","Возраст:", nameAge[5]);
-console.log("Имя:",nameAge[6], ",","Возраст:", nameAge[7]);
-console.log("Имя:",nameAge[8], ",","Возраст:", nameAge[9]);
+// //создайте массив имен
+// //создайте массив типа ["Имя" возраст]["Movsar", 25, Adam, 27...]
+// let nameAge = ['Movsar', 32, 'Said', 11, 'Ibra', 15, 'Muhammad', 44, 'Adlan', 27]
+// console.log("Имя:",nameAge[0], ",","Возраст:", nameAge[1]);
+// console.log("Имя:",nameAge[2], ",","Возраст:", nameAge[3]);
+// console.log("Имя:",nameAge[4], ",","Возраст:", nameAge[5]);
+// console.log("Имя:",nameAge[6], ",","Возраст:", nameAge[7]);
+// console.log("Имя:",nameAge[8], ",","Возраст:", nameAge[9]);
+
+// let myname = "Movsar"
+// let age = 25
+
+// let myFriendName = "Ado"
+// let car = "Camry 3.5"
+
+// let myCar = car
+
+// console.log("Моя машина:", myCar, ",", "такая же как у", myFriendName);
+
+// let namesAge = [
+//     ["Movsar", 25],
+//     ["Movlid", 24],
+//     ["Ibra", 26],
+//     ["Said", 30],
+//     ["Adam", 23]
+// ]
+// console.log("Меня зовут:", namesAge[0][0], "мне", namesAge[0][1], "года")
+// console.log("Моего друга зовут: "+namesAge[1][1]+ ", ему "+ namesAge[1][1]+ " года")
+
+// let str = "I live in Grozny"
+// console.log(str[0])
+// console.log(str[1])
+// console.log(str[2])
+// console.log(str[3])
+// console.log(str[4])
+// console.log(str[5])
+// console.log(str[6])
+// console.log(str[7])
+// console.log(str[8])
+// console.log(str[9])
+// console.log(str[10])
+// console.log(str[11])
+// console.log(str[12])
+// console.log(str[13])
+// console.log(str[14])
+// console.log(str[15])
+// let index = 0
+
+// let religion= "Islam"
+// let str = "muslim"
+
+// console.log(religion[index]);
+// console.log(religion[index+1]);
+// console.log(religion[index+2]);
+// console.log(religion[index+3]);
+// console.log(religion[index+4]);
+//
+// let index = 0
+// let religion= "Islam"
+// let text= "Chechen republic center world"
+// let names = [["Movsar"], ['Movlid'],['Said'],['Ibra'],['Adam']]
+// let num = [1,2,3,4,5,6,7,8]
+// console.log(text.length);
+// console.log(names.length);
+// console.log(num.length ); 
+console.log(3<5);
+console.log(3>5);
+console.log(3<=3);
+console.log(3>=2);
+
+console.log("ab"==="ab");
+console.log("ad"!=="ab");
+console.log("ad"==="ad");
